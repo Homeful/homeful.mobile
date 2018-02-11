86273b477534f408f4ffa4c36d2f101523f5fa9b
Conner Bush <connerbush@gmail.com>
2018-02-10 22:04:24 -0600

    adds location saving to camp creation

--- Homeful.mobile/Views/Camp/NewCampPage.xaml.cs
+++ Homeful.mobile/Views/Camp/NewCampPage.xaml.cs
@@ -1,29 +1,44 @@
 ﻿using System;
-
+using Plugin.Geolocator;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace Homeful.mobile
 {
     public partial class NewCampPage : ContentPage
     {
         public Camp Camp { get; set; }
+        MapSpan mapSpan;
 
         public NewCampPage()
         {
             InitializeComponent();
 
-            Camp = new Camp
-            {
-                Name = "Camp name"
-            };
+            Camp = new Camp();
 
             BindingContext = this;
         }
 
         async void Save_Clicked(object sender, EventArgs e)
         {
+            Camp.Location = new Location()
+            {
+                Lat = mapSpan.Center.Latitude,
+                Lng = mapSpan.Center.Longitude
+            };
             MessagingCenter.Send(this, "AddCamp", Camp);
             await Navigation.PopToRootAsync();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            var locator = CrossGeolocator.Current;
+
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+            var currentLocation = new Position(position.Latitude, position.Longitude);
+            mapSpan = MapSpan.FromCenterAndRadius(currentLocation, Distance.FromMeters(500));
+            MyMap.MoveToRegion(mapSpan);
         }
     }
 }
--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -13,9 +13,8 @@ namespace Homeful.mobile.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Xamarin.FormsMaps.Init();
             LoadApplication(new App());
-
-
 
             return base.FinishedLaunching(app, options);
         }
